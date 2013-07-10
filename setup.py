87590f249a223f28960da489ae6244e48349dbd6
Joost Molenaar <j.j.molenaar@gmail.com>
2013-07-10 08:12:16 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+from distutils.core import setup
+setup(
+    author='Joost Molenaar',
+    author_email='j.j.molenaar@gmail.com',
+    name='oauth',
+    packages=['oauth'],
+    url='https://github.com/j0057/oauth',
+    version='0.1.0'
+)
+
+
