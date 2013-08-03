e06fc25eef04772e9c2a517be4f21daaab627429
Joost Molenaar <j.j.molenaar@gmail.com>
2013-08-03 10:48:29 +0200

    Rename jjm.mp3 to mp3

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from distutils.core import setup
 setup(
     author='Joost Molenaar',
     author_email='j.j.molenaar@gmail.com',
-    name='jjm.mp3',
-    packages=['jjm.mp3'],
-    url='https://github.com/JoostMolenaar/jjm.mp3',
+    name='mp3',
+    packages=['mp3'],
     version='0.1.0'
 )
 
