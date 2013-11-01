d231091125cc379a31804b34215b64a1aa9c94ce
Joost Molenaar <j.j.molenaar@gmail.com>
2013-11-01 10:21:24 +0100

    Update presentation

--- static/pres.js
+++ static/pres.js
@@ -1,23 +1,91 @@
-(function($q, $qa) {
+(function($q, $qa, $hash) {
     document.addEventListener("DOMContentLoaded", function(e) {
-        var state = 0;
-        var states = [
-            function() { $q("#slide-0").style.display = "block"; },
-            function() { $q("#slide-1").style.display = "block"; },
-            function() { $q("#slide-2").style.display = "block"; }
-        ];
+        var slides = $qa(".slide");
+        var slideCount = slides.length;
+        var slideMaxState;
+        var hash = {};
 
-        var setState = function(newState) {
-            $qa(".slide").map(function(slide) { slide.style.display = "none"; });
-            states[state = newState]();
+        var states = {
+            3: function(slide, state) {
+                $qa("dt", $qa(".slide")[slide]).map(function(dt, i) {
+                    dt.style.display = i < state ? "block" : "none";
+                });
+                $qa("dd", $qa(".slide")[slide]).map(function(dd, i) {
+                    dd.style.display = i < state ? "block" : "none";
+                });
+            },
+            6: function(slide, state) {
+                $qa("g", $qa(".slide")[slide]).map(function(g, i) {
+                    g.style.display = i < state ? "block" : "none";
+                });
+            }
+        };
+
+        var next = function() {
+            if (hash.state < slideMaxState) {
+                $hash.set({ slide: hash.slide, state: hash.state + 1 });
+            }
+            else if (hash.slide < (slides.length - 1)) {
+                $hash.set({ slide: hash.slide + 1, state: 0 });
+            }
+        };
+        var prev = function() {
+            if (hash.state > 0) { 
+                $hash.set({ slide: hash.slide, state: hash.state - 1 }); 
+            }
+            else if (hash.slide > 0) { 
+                $hash.set({ slide: hash.slide - 1, state: 0 }); 
+            }
+        };
+
+        var processHash = function(e) {
+            var newHash = $hash.get();
+            newHash.slide = Number(newHash.slide);
+            newHash.state = Number(newHash.state);
+            if (hash.slide != newHash.slide) {
+                slides.map(function(slide) { slide.style.display = 'none'; });
+                slides[newHash.slide].style.display = 'block';
+                slideMaxState = Number(slides[newHash.slide].getAttribute("data-states"));
+            };
+            hash = newHash;
+            if (states[hash.slide]) { 
+                states[hash.slide](hash.slide, hash.state);
+            }
         };
 
         window.addEventListener("keydown", function(e) {
-            if (e.keyIdentifier == "Left" && state > 0)
-                setState(state - 1);
-            else if (e.keyIdentifier == "Right" && state < (states.length - 1))
-                setState(state + 1);
+            console && console.log && console.log(e, e.keyIdentifier, e.keyCode);
+            switch (e.keyIdentifier) {
+                case "Left":
+                    prev();
+                    break;
+                case "Right":
+                    next();
+                    break;
+            }
         });
+
+        window.addEventListener("mousedown", function(e) {
+            console && console.log && console.log(e, e.button);
+            switch (e.button) {
+                case 2:
+                    //prev();
+                    //e.preventDefault();
+                    break;
+                case 0:
+                    next();
+                    break;
+            }
+        });
+
+        //window.addEventListener("contextmenu", function(e) { e.preventDefault(); });
+        window.addEventListener("hashchange", processHash);
+
+        if (!location.hash) { 
+            $hash.set({ slide: 0, state: 0 });
+        } else {
+            processHash();
+        }
 });
 })( function(s, ctx) {
         ctx = ctx || document;
@@ -26,4 +94,23 @@
     function(s, ctx) {
         ctx = ctx || document;
         return Array.prototype.slice.call(ctx.querySelectorAll(s));
+    },
+    {
+        get: function() {
+            var result = {};
+            location.hash
+                .substr(1)
+                .split("&")
+                .map(function(kv) { return kv.split("=", 2); })
+                .map(function(kv) { result[kv[0]] = decodeURI(kv[1]); });
+            return result;
+        },
+        set: function(values) {
+            var hash = "";
+            for (var k in values) {
+                if (hash) hash += "&";
+                hash += k + "=" + encodeURI(values[k]);
+            }
+            location.hash = hash;
+        }
     });
